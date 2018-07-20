4f6c277245e9003079c9c095b0ce4861ce015342
chenhw78 <chenhw78@chinaunicom.cn>
2018-07-20 18:29:32 +0800

    1

--- SuningOrder/src/test/java/com/cjh/suning/test/ChromeDriverTest.java
+++ SuningOrder/src/test/java/com/cjh/suning/test/ChromeDriverTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -17,6 +18,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ResourceUtils;
 
 import com.google.common.collect.Maps;
 
@@ -27,42 +29,90 @@ public class ChromeDriverTest {
 	@Test
 	public void testLogin() {
 		long startTime = System.currentTimeMillis();
-		System.setProperty("webdriver.chrome.driver", "E:\\git\\SuningOrder\\src\\main\\resources\\chromedriver.exe"); // 指定驱动路径
-		ChromeOptions chromeOptions = new ChromeOptions();
-		// 无头模式
-		// chromeOptions.addArguments("--headless");
-		/** begin 禁止加载图片 **/
-		Map<String, Object> contentSettings = Maps.newHashMap();
-		contentSettings.put("images", 2);
-		Map<String, Object> preferences = Maps.newHashMap();
-		preferences.put("profile.default_content_setting_values", contentSettings);
-		chromeOptions.setExperimentalOption("prefs", preferences);
-		/** end **/
+		WebDriver driver = null;
+		try {
+			File file = ResourceUtils.getFile("classpath:chromedriver.exe");
+			String driverPath = "";
+			if (file.exists()) {
+				driverPath = file.getPath();
+			}
+			System.setProperty("webdriver.chrome.driver", driverPath); // 指定驱动路径
+			ChromeOptions chromeOptions = new ChromeOptions();
+			// 无头模式
+			// chromeOptions.addArguments("--headless");
+			/** begin 禁止加载图片 **/
+			Map<String, Object> contentSettings = Maps.newHashMap();
+			contentSettings.put("images", 2);
+			Map<String, Object> preferences = Maps.newHashMap();
+			preferences.put("profile.default_content_setting_values", contentSettings);
+			chromeOptions.setExperimentalOption("prefs", preferences);
+			/** end **/
 
-		WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("https://order.suning.com/onlineOrder/orderList.do");
-		// wait.until(ExpectedConditions.titleContains("用户登录"));
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		wait.until(isPageLoaded());
-		JavascriptExecutor j = (JavascriptExecutor) driver;
-		j.executeScript("document.getElementsByClassName('pc-login')[0].style.display='block';");
-		driver.findElement(By.id("userName")).sendKeys(new String[] { "185777077201" });
-		driver.findElement(By.id("password")).sendKeys(new String[] { "2hb7110chw3" });
-		driver.findElement(By.id("submit")).click();
-		wait.until(isPageLoaded());
-		System.out.println(driver.getCurrentUrl());
+			driver = new ChromeDriver(chromeOptions);
+			driver.get("https://order.suning.com/onlineOrder/orderList.do");
+			// wait.until(ExpectedConditions.titleContains("用户登录"));
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(isPageLoaded());
+			JavascriptExecutor j = (JavascriptExecutor) driver;
+			j.executeScript("document.getElementsByClassName('pc-login')[0].style.display='block';");
+			driver.findElement(By.id("userName")).sendKeys(new String[] { "1857771287720" });
+			driver.findElement(By.id("password")).sendKeys(new String[] { "hb2231710cchw" });
+			driver.findElement(By.id("submit")).click();
+			wait.until(isPageLoaded());
+			System.out.println(driver.getCurrentUrl());
+			do {
+				driver.get("https://product.suning.com/0000000000/690105206.html");
+				wait.until(isPageLoaded());
 
-		long endTime = System.currentTimeMillis();
-		float excTime = (float) (endTime - startTime) / 1000;
-		System.out.println("执行时间：" + excTime + "s");
-		Scanner input = new Scanner(System.in);
-		String val = null; // 记录输入的字符串
-		do {
-			val = input.next(); // 等待输入值
-		} while (!val.equals("q")); // 如果输入的值不是#就继续输入
-		System.out.println("你输入了\"q\"，程序已经退出！");
-		input.close(); // 关闭
-		driver.quit();
+				WebElement webElement = driver.findElement(By.cssSelector(".clr-item[title='红色']"));
+				if (webElement != null) {
+					if (!webElement.getAttribute("class").contains("selected")) {
+						webElement.findElement(By.cssSelector("a")).click();
+					}
+				}
+				wait.until(isPageLoaded());
+				webElement = driver.findElement(By.cssSelector("#versionItemList>dd>ul>li[title='64G公开版']"));
+				if (webElement != null) {
+					if (webElement.getAttribute("class").contains("disabled")) {
+						System.out.println("现在还买不了64G公开版");
+						break;
+					}
+					if (!webElement.getAttribute("class").contains("selected")) {
+						webElement.findElement(By.cssSelector("a")).click();
+					}
+				}
+				webElement = driver.findElement(By.cssSelector("#phoned1"));
+				if (webElement == null) {
+					System.out.println("现在还买不不能购买");
+					break;
+				}else{
+					if (webElement.getCssValue("display").equals("none")) {
+						System.out.println("现在还买不不能购买");
+						break;
+					}
+				}
+				webElement = webElement.findElement(By.cssSelector("dd>ul>li[title='裸机版']"));
+				if (!webElement.getAttribute("class").contains("selected")) {
+					webElement.findElement(By.cssSelector("a")).click();
+				}
+			} while (false);
+			long endTime = System.currentTimeMillis();
+			float excTime = (float) (endTime - startTime) / 1000;
+			System.out.println("执行时间：" + excTime + "s");
+			Scanner input = new Scanner(System.in);
+			String val = null;
+			do {
+				val = input.next();
+			} while (!val.equals("q"));
+			System.out.println("你输入了\"q\"，程序已经退出！");
+			input.close();
+			driver.quit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (driver != null) {
+				driver.quit();
+			}
+		}
 	}
 
 	protected Function<WebDriver, Boolean> isPageLoaded() {
